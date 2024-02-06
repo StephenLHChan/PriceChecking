435ecb2328b83ad6be537eb6473199c39a1944dd
StephenLHChan <stephenc.1018@gmail.com>
2024-02-06 14:00:25 -0500

    feat(cdk): lambda init

--- infra/lib/backend-stack.ts
+++ infra/lib/backend-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as log from 'aws-cdk-lib/aws-logs';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 import { Construct } from 'constructs';
@@ -43,6 +46,41 @@ export class BackendStack extends cdk.Stack {
 		new ssm.StringParameter(this, 'PrivateSubnetIdParameter', {
 			parameterName: `/${appName}/PrivateSubnetId`,
 			stringValue: vpc.privateSubnets[0].subnetId,
+		});
+
+		const lambdaRole = new iam.Role(this, 'lambdaRole', {
+			assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+		});
+		lambdaRole.addManagedPolicy(
+			iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambdaBasicExecutionRole')
+		);
+		lambdaRole.addManagedPolicy(
+			iam.ManagedPolicy.fromAwsManagedPolicyName(
+				'AWSLambdaVPCAccessExecutionRole'
+			)
+		);
+
+		const lambdaLogGroup = new log.LogGroup(
+			this,
+			'express-serverless-lambda-log-group',
+			{
+				logGroupName: `/aws/lambda/${appName}`,
+				retention: log.RetentionDays.TWO_WEEKS,
+			}
+		);
+
+		new lambda.Function(this, 'express-serverless-lambda', {
+			functionName: `${appName}`,
+			runtime: lambda.Runtime.NODEJS_LATEST,
+			handler: 'index.handler',
+			code: lambda.Code.fromAsset('lambda'),
+			role: lambdaRole,
+			timeout: cdk.Duration.seconds(30),
+			vpc,
+			vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+			logGroup: lambdaLogGroup,
+			systemLogLevel: lambda.SystemLogLevel.INFO,
+			applicationLogLevel: lambda.ApplicationLogLevel.INFO,
 		});
 	}
 }
