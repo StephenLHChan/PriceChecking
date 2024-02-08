869b5042ef4e051ae70f672e8926865ba9699bbe
StephenLHChan <stephenc.1018@gmail.com>
2024-02-07 21:30:31 -0500

    feat(cdk): APIGateway init

--- infra/lib/constructs/ExpressApi.ts
+++ infra/lib/constructs/ExpressApi.ts
@@ -1,8 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 
+import { Cors } from 'aws-cdk-lib/aws-apigateway';
+import {
+	CorsHttpMethod,
+	HttpApi,
+	HttpMethod,
+} from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as log from 'aws-cdk-lib/aws-logs';
 
 import { Construct } from 'constructs';
@@ -12,6 +21,7 @@ import { appName } from '../vars';
 interface ExpressApiProps {
 	vpc: ec2.Vpc;
 }
+
 export class ExpressApi extends cdk.Stack {
 	constructor(scope: Construct, id: string, props: ExpressApiProps) {
 		super(scope, id);
@@ -37,18 +47,47 @@ export class ExpressApi extends cdk.Stack {
 			}
 		);
 
-		new lambda.Function(this, 'express-serverless-lambda', {
-			functionName: `${appName}`,
-			runtime: lambda.Runtime.NODEJS_LATEST,
-			handler: 'index.handler',
-			code: lambda.Code.fromAsset('lambda'),
-			role: lambdaRole,
-			timeout: cdk.Duration.seconds(30),
-			vpc: props.vpc,
-			vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
-			logGroup: lambdaLogGroup,
-			systemLogLevel: lambda.SystemLogLevel.INFO,
-			applicationLogLevel: lambda.ApplicationLogLevel.INFO,
+		const lambdaFunction = new NodejsFunction(
+			this,
+			'express-serverless-lambda',
+			{
+				functionName: `${appName}`,
+				runtime: lambda.Runtime.NODEJS_LATEST,
+				handler: 'index.handler',
+				role: lambdaRole,
+				timeout: cdk.Duration.seconds(30),
+				vpc: props.vpc,
+				vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+				logGroup: lambdaLogGroup,
+				systemLogLevel: lambda.SystemLogLevel.INFO,
+				applicationLogLevel: lambda.ApplicationLogLevel.INFO,
+			}
+		);
+		const integration = new HttpLambdaIntegration(
+			'LambdaIntegration',
+			lambdaFunction
+		);
+
+		const api = new HttpApi(this, 'HttpApi', {
+			apiName: 'Price-Checking',
+			corsPreflight: {
+				allowHeaders: ['*'],
+				allowMethods: [CorsHttpMethod.ANY],
+				allowOrigins: Cors.ALL_ORIGINS,
+				maxAge: cdk.Duration.hours(24),
+			},
+		});
+		api.addRoutes({
+			path: '/{proxy+}',
+			integration,
+			methods: [
+				HttpMethod.HEAD,
+				HttpMethod.GET,
+				HttpMethod.POST,
+				HttpMethod.PATCH,
+				HttpMethod.PUT,
+				HttpMethod.DELETE,
+			],
 		});
 	}
 }
