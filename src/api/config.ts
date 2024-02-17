551a588efd9e76a14f57d2bf1cfab07441ceaa83
StephenLHChan <stephenc.1018@gmail.com>
2024-02-17 11:31:18 -0500

    feat(express): add config file

---
+++ src/api/config.ts
@@ -0,0 +1,4 @@
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+export const USER_TABLE = process.env.USER_TABLE as string;
+export const PRICE_TABLE = process.env.PRICE_TABLE as string;
+export const PRODUCT_TABLE = process.env.PRODUCT_TABLE as string;
